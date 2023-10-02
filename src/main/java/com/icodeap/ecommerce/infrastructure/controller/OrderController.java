bb5439ba086b065e2b2cd7d928da7c9afb36ec04
Diegojzc <djavi91_05@hotmail.com>
2023-10-02 11:08:21 +0200

    carrito generar

--- src/main/java/com/icodeap/ecommerce/infrastructure/controller/OrderController.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/controller/OrderController.java
@@ -1,23 +1,41 @@
 package com.icodeap.ecommerce.infrastructure.controller;
 
-import com.icodeap.ecommerce.application.service.CartService;
-import com.icodeap.ecommerce.application.service.UserService;
-import com.icodeap.ecommerce.domain.User;
+import com.icodeap.ecommerce.application.service.*;
+import com.icodeap.ecommerce.domain.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/user/order")
+@Slf4j
 public class OrderController {
 
     private final CartService cartService;
     private final UserService userService;
+    private final ProductService productService;
+    private final OrderService orderService;
+    private final OrderProductService orderProductService;
+    private final StockService stockService;
+    private final ValidateStock validateStock;
 
-    public OrderController(CartService cartService, UserService userService) {
+
+    public OrderController(CartService cartService, UserService userService, ProductService productService,
+                           OrderService orderService, OrderProductService orderProductService,
+                           StockService stockService, ValidateStock validateStock) {
         this.cartService = cartService;
         this.userService = userService;
+        this.productService = productService;
+        this.orderService = orderService;
+        this.orderProductService = orderProductService;
+        this.stockService = stockService;
+        this.validateStock = validateStock;
     }
 
     @GetMapping("/sumary-order")
@@ -27,5 +45,42 @@ public class OrderController {
         model.addAttribute("total", cartService.getTotalCart());
         model.addAttribute("user",user);
         return "user/sumaryorder";
+    }
+
+    @GetMapping("/create-order")
+    public String createOrder(){
+        log.info("create order..");
+        //obetener user temporal
+        User user = userService.findById(1);
+        //order
+        Order order = new Order();
+        order.setDateCreated(LocalDateTime.now());
+        order.setUser(user);
+        order = orderService.createOrder(order);
+        //order product
+        List<OrderProduct> orderProduct = new ArrayList<>();
+
+        // itemcart= orderProduct
+        for (ItemCart itemCart : cartService.getItemCarts()){
+            orderProduct.add(new OrderProduct(productService.getProductById(itemCart.getIdProduct()),
+                    itemCart.getQuantity(), order));
+
+        }
+        //
+        orderProduct.forEach(
+                op->{orderProductService.create(op);
+                    Stock stock = new Stock();
+                    stock.setDateCreated(LocalDateTime.now());
+                    stock.setProduct(op.getProduct());
+                    stock.setDescription("venta");
+                    stock.setUnitIn(0);
+                    stock.setUnitOut(op.getQuantity());
+                    stockService.saveStock(validateStock.calculateBalance(stock));
+                }
+
+        );
+        cartService.removeAllItemsCart();
+
+        return "redirect:/home";
     }
 }
